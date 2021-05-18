0343aeca552042301691b02be7e5f9694a2eca12
Konrad Fedorczyk <contact@realhe.ro>
2021-05-18 13:30:15 +0200

    Rename iptc test

---
+++ tests/IptcTest.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OKO\IptcMapper\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Exception;
+
+use OKO\IptcMapper\ImageInfo;
+use OKO\IptcMapper\Iptc;
+use OKO\IptcMapper\Tests\ImageInfoTest;
+
+final class IptcTest extends TestCase 
+{
+    private $imageInfo;
+
+    protected function setUp(): void
+    {
+        $this->imageInfo = new ImageInfo(ImageInfoTest::TEST_FILE);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->imageInfo = NULL;
+    }
+
+    public function testExtraction(): void
+    {
+        $itpcMap = new Iptc($this->imageInfo);
+
+        $rawData = $itpcMap->getRawData();
+         
+        $this->assertEquals(true, is_array($rawData));
+    }
+}
